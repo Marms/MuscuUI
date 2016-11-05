c241982795782848b1e56e353bf878457b8386f6
Florent Delmotte <delmotteflorent@sfr.fr>
2016-11-05 15:44:24 +0100

    Ajout update ExoTemplate + filtre sur type exo

--- src/main/resources/public/js/app/pages/exo-template/exo-template.controller.js
+++ src/main/resources/public/js/app/pages/exo-template/exo-template.controller.js
@@ -2,18 +2,80 @@
 
 angular.module('DashboardWM')
     .controller('ExoTemplateController', function ($scope, exoPredefService) {
-    	$scope.getList = function() {
+    	var listView = $scope.listView = [
+            "LISTE_EXO_TEMPLATE",
+            "CREATE_UPDATE_EXO_TEMPLATE"
+	    ];
+
+    	var typeExo = $scope.typeExo = [           
+	    	{id:"0", value : "ABDO"},
+			{id:"1", value : "PECTORAUX"},
+			{id:"2", value : "JAMBE"},
+			{id:"3", value : "DOS"},
+			{id:"4", value : "MOLLET"},
+			{id : "5", value : "BRAS"}
+		];
+    	//affiche tout les exo
+		
+    	$scope.initexoShown = function() {    		
+    		$scope.exoShown = ["ABDO", "PECTORAUX", "JAMBE", "DOS", "MOLLET", "BRAS", ""];
+    		$scope.exoHidden = [];
+    	}
+    	
+    	$scope.initexoShown();
+    	$scope.changeView = function(index) {
+    		$scope.exoTemplate = {};
+    		console.log("change view " + listView[index]);
+    		$scope.actualView = listView[index];
+    	}
+    	$scope.changeView(0);
+    	
+        $scope.getList = function() {
     		exoPredefService.getList().success(function(data) {
     			$scope.exoTemplates = data;
     		});
-    		
+    	};
+    	
+    	$scope.hideExo = function(id) {
+    		var index = $scope.exoShown.indexOf(id);
+    		if (index >= 0) {
+    			$scope.exoShown.splice(index, 1);
+    			$scope.exoHidden.push(id);
+    		}
+    		console.log("exoShown " + $scope.exoShown);
     	}
+    	$scope.showExo = function(exo) {
+    		var index = $scope.exoHidden.indexOf(exo);
+    		if (index >= 0) {
+    			$scope.exoHidden.splice(index,1);
+    		}
+    		$scope.exoShown.push(exo);
+    	}
+    	/** renvoi vrai si le type exo est egale au exo a voir*/
+    	$scope.showLine = function(e) {
+    		if (e.type.length == 0) {
+    			// cas des exo n'ayant pas de type definie
+    			return true;
+    		}
+    		for (var i =0; i < e.type.length ; i++) {
+    			if ($scope.exoShown.indexOf(e.type[i]) >= 0) {
+    				return true;
+    			}
+    		}
+    		return false;
+    	}
+ 
   	   $scope.submitCreateExoTemplate = function() {
 			exoPredefService.saveExoPredef($scope.exoTemplate).success(function(data){
-				$scope.getList()
+				$scope.getList();
+				$scope.changeView(0);
 			});
 	    }
-    	
+  	   
+  	   $scope.updateExoTemplate = function(e) {
+  		   $scope.exoTemplate = e;
+  		   $scope.changeView(1);
+  	   }
   	   $scope.getList();
   	   $scope.gridExoTemplates = {
   	   	   data: 'exoTemplates'
