8cf8ffd484f976c6d0e969848ffeebdbff11fdfc
Florent Delmotte <delmotteflorent@sfr.fr>
2017-01-29 21:40:00 +0100

    Refacto Seance

--- src/main/resources/public/js/app/pages/seance/seance.controller.js
+++ src/main/resources/public/js/app/pages/seance/seance.controller.js
@@ -1,23 +1,12 @@
 'use strict';
 
 angular.module('DashboardWM')
-    .controller('SeanceController', function ($scope, seanceService, seancePredefService, exoPredefService) {
+    .controller('SeanceController', function ($scope, $location, seanceService, seancePredefService, exoPredefService) {
     	var listView = $scope.listView  = [
     	    "LISTE_SEANCE",
-    		"LISTE_SEANCE_PREDEF",
-    		"LISTE_EXO_TEMPLATE",
-    		"SELECTIONNER_EXO",
-    		"LISTE_SERIE",
-    		"ADD_OTHER_EXO"
+    		"LISTE_SEANCE_PREDEF"
     	];
-    	var EXO_DONE = "panel-red";
-   
-     	$scope.initexoShown = function() {    		
-    		$scope.exoShown = ["ABDO", "PECTORAUX", "JAMBE", "DOS", "MOLLET", "BRAS"];
-    		$scope.exoHidden = [];
-    	}
-    	$scope.initexoShown();
-
+    	
     	$scope.changeView = function(index) {
     		console.log("view " + listView[index]);
     		$scope.actualView = listView[index];
@@ -26,13 +15,6 @@ angular.module('DashboardWM')
     	$scope.changeView(0);
 
     	// RECUPERATION DES DONNEES
-        $scope.getExoTemplate = function() {
-        	exoPredefService.getList().success(function(data) {
-        		$scope.exoPredefs = data;        		
-        	});
-        }
-        $scope.getExoTemplate();
-        
         $scope.getSeanceTemplates = function() {
         	seancePredefService.getList().success(function(data) {
         		$scope.seanceTemplates = data;
@@ -49,57 +31,18 @@ angular.module('DashboardWM')
  
         // FUNCTION DE L APPLICATION
         $scope.choixSeance = function(seanceTemplate) {
+        	console.log("creation d une seance");
         	$scope.seance = {};
         	$scope.seance.seancePredef = seanceTemplate;
         	$scope.seance.exercices = []; //init liste vide
-        	$scope.affichageExo($scope.seance);
-        	$scope.changeView(2);
-        }
-
-        $scope.saveSeance = function() {
-        	seanceService.save($scope.seance).success( function(data) {
-        		$scope.seance = data;
-        		$scope.seances.push($scope.seance);
-        		$scope.changeView(0);
-                $scope.otherExo = [];
+        	seanceService.save($scope.seance).success(function(data) {
+        		$scope.updateSeance(data);
         	});
         }
+
         $scope.updateSeance = function(s) {
-        	$scope.seance = s;
-        	$scope.seancePredef = s.seancePredef;
-        	$scope.affichageExo(s);
-        	$scope.changeView(2);
-        	console.log(s);
-        }
-
-        $scope.choixOtherExo = function(exo) {
-        	$scope.choixExo(exo); // creation d un nouvau exo
-        	$scope.affichageExo($scope.seance);
-        	$scope.changeView(2);        	
-        }
-
-        //ajoute les exercice de la seance a la liste affichageExo:
-        $scope.affichageExo = function(s) {
-        	$scope.exos = s.seancePredef.list;
-        	// ajout des exercices supplementaires + modification css
-        	console.log("in exo " + $scope.exos);
-        	
-        	for (var indexExo =0; indexExo < s.exercices.length; indexExo++) {
-        		var ajouter = true;
-        		for (var i = 0; i < $scope.exos.length; i++) {
-        			if ($scope.exos[i].name === s.exercices[indexExo].exoPredef.name) {
-        				ajouter = false;
-        				// changement de la couleur du panel:
-        				$scope.exos[i].done = EXO_DONE;
-        				// TODO ajouter les series au panel
-        				$scope.exos[i].panelBody = $scope.createPanelBody(s.exercices[indexExo]);
-        			}
-        		}
-        		if (ajouter) {
-        			$scope.exos.push(s.exercices[indexExo].exoPredef);
-        			//s.exercices[indexExo].done = EXO_DONE;
-        		}
-        	}
+        	console.log("update d une seance");
+        	$location.path("/seance/"+s.id + "/exercice");
         }
         
         $scope.createPanelBody = function (exo) {
@@ -124,93 +67,15 @@ angular.module('DashboardWM')
     		// 
     		
     	};
-        /* modifie actualExo
-         * - si exo non present dans les exercices de la seance en cours initialisation nouveau exo
-         * - sinon recuperation de l object correspondant
-         * */
-        $scope.choixExo = function(exo) {
-        	$scope.getOldSeries(exo);
-        	$scope.numero = 0;
-        	if (exo.done === EXO_DONE) {
-        		$scope.actualExo = $scope.seance.exercices.filter(function(data) {
-        			return data.exoPredef.id == exo.id;
-        		})[0];
-        		$scope.actualExo.done = EXO_DONE;
-        		// Todo recuperation du numero de serie
-								//        		for(var serie in $scope.actualExo.series) { //TODO doesnt work 
-								//        			if ($scope.numero < serie.numero) {
-								//        				$scope.numero = serie.numero;
-								//        			}
-								//        		}
-        	} else {
-        		$scope.actualExo = {};        		
-        		$scope.actualExo.series = [];
-        		$scope.actualExo.exoPredef = exo; //set exotemplate
-        		$scope.actualExo.date = new Date();
-        		console.log("date exo " + $scope.actualExo.date);
-        		$scope.seance.exercices.push($scope.actualExo);
-        	}
-        	$scope.initSerie();
-        	exo.done = EXO_DONE;
-        	$scope.changeView(4);
-        	$scope.gridSeries = {
-            		data: 'actualExo.series'
-            }
-        };
-        
- 
-        $scope.initSerie = function() {
-        	console.log("initSerie");
-        	$scope.serie = {};
-        	$scope.serie.nbRepeat = 10;
-        }
-
-        // ajout de la serie a la liste de series de l'exercice actuel
-        $scope.addSerie = function() {
-        	$scope.serie.numero = $scope.numero;
-        	$scope.numero++;
-        	$scope.actualExo.series.push($scope.serie);
-        	// mise a zero du formulaire
-        	$scope.initSerie();
-        };
-
-    	$scope.hideExo = function(id) {
-    		var index = $scope.exoShown.indexOf(id);
-    		if (index >= 0) {
-    			$scope.exoShown.splice(index, 1);
-    			$scope.exoHidden.push(id);
-    		}
-    		console.log("exoShown " + $scope.exoShown);
-    	}
-    	$scope.showExo = function(exo) {
-    		var index = $scope.exoHidden.indexOf(exo);
-    		if (index >= 0) {
-    			$scope.exoHidden.splice(index,1);
-    		}
-    		$scope.exoShown.push(exo);
-    	}
-    	/** renvoi vrai si le type exo est egale au exo a voir*/
-    	$scope.showLine = function(e) {
-    		if (e.type.length == 0) {
-    			// cas des exo n'ayant pas de type definie
-    			return true;
-    		}
-    		for (var i =0; i < e.type.length ; i++) {
-    			if ($scope.exoShown.indexOf(e.type[i]) >= 0) {
-    				return true;
-    			}
-    		}
-    		return false;
-    	};
     	
-    	$scope.open = function($event) {
-             $event.preventDefault();
-             $event.stopPropagation();
-
-             $scope.obj = {
-                  opened : true
-             };
-       };
+//    	$scope.open = function($event) {
+//             $event.preventDefault();
+//             $event.stopPropagation();
+//
+//             $scope.obj = {
+//                  opened : true
+//             };
+//       };
     });
 
 
@@ -219,7 +84,6 @@ angular.module('DashboardWM').service('seanceService', function($http) {
 	this.save = function(seance) {
 		return $http.post('/v1/seance', seance);
 	}
-
 	this.getSeance = function(scId) {
 		return $http.get('/v1/seance/'+ scId);
 	}
