cf081e8b42b184e8b72f1c76ce2dfc6a03939313
Florent Delmotte <delmotteflorent@sfr.fr>
2017-02-04 19:03:02 +0100

    Serie: ajout update/delete d'une serie

--- src/main/resources/public/js/app/pages/serie/serie.controller.js
+++ src/main/resources/public/js/app/pages/serie/serie.controller.js
@@ -20,7 +20,11 @@ angular.module('DashboardWM')
         // ajout de la serie a la liste de series de l'exercice actuel
         $scope.addSerie = function() {
         	// mise a zero du formulaire
-        	serieService.save(scId, exoId, $scope.serie);
+        	serieService.save(scId, exoId, $scope.serie).success(function(data) {
+        		$scope.serie=data;
+        		console.log("numero " + data.numero);
+        	});
+        	$scope.serie.numero = $scope.actualExo.series.length; //TODO pourquoi numero non set precedement ?
         	$scope.actualExo.series.push($scope.serie);
         	$scope.initSerie();
         };
@@ -48,12 +52,40 @@ angular.module('DashboardWM')
     		$rootScope.actualExo = {};
     		$location.path("seance/" + scId + "/exercice");
     	}
+    	
+    	$scope.open = function(serie) {
+    		$scope.userie = serie;
+    	};
+
+		$scope.update = function() {
+			console.log("update serie");
+			//update d'une serie
+			serieService.update(scId, exoId, $scope.userie);
+			$scope.userie = {};
+		};
+
+		$scope.deleteSerie = function() {
+			serieService.removeSerie(scId, exoId, $scope.userie).success(function(data) {
+				$scope.actualExo = data;
+			});
+		}
+		$scope.cancel = function() {
+			console.log("close modal");
+    		  $scope.showModal = false;
+		};
 
     });
 angular.module('DashboardWM').service('serieService', function($http) {
 	// Makes the REST request to get the data to populate the grid.
 	this.save = function(scId, exId, serie) {
 		return $http.post('/v1/seance/'+scId+ '/exercice/'+ exId +'/serie', serie);
+	}
+	// Makes the REST request to get the data to populate the grid.
+	this.update = function(scId, exId, serie) {
+		return $http.patch('/v1/seance/'+scId+ '/exercice/'+ exId +'/serie/'+ serie.numero, serie);
+	}
+	this.removeSerie = function(scId, exId, serie) {
+		return $http.delete('/v1/seance/'+scId+ '/exercice/'+ exId +'/serie/'+ serie.numero);
 	}
 
 	this.getList = function() {
